f40cc8d66061bfe65448ffd497eb689fc6abddb6
priscilafaliani <priscilafaliani@gmail.com>
2020-10-11 13:43:59 -0300

    >-<

--- Practica 2/Ejercicio12.asm
+++ Practica 2/Ejercicio12.asm
@@ -7,13 +7,33 @@ TIMERID EQU 25
 RCONT EQU 10h
 RCOMP EQU 11h
 
-; variables
+; reloj con formato 00:00
 org 1000h
-segundos db 0 
+minutos1 db 30h
+minutos2 db 30h
+semicolon db ":"
+segundos1 db 30h
+segundos2 db 30h
+enterA db 0Dh
+finReloj db ?
 
 ; timer subroutine
 org 3000h
-timerSR: int 7
+; si llego al 00:09 (o similar)
+timerSR: cmp segundos2, 39h
+jz checkSegundos1
+inc segundos2
+jmp print
+; si llego al 00:59
+checkSegundos1: cmp segundos1, 35h
+jz fin
+; si no, incrementa 00:*0*0 y reinicia 00:0*0*
+inc segundos1
+mov segundos2, 30h
+print: int 7
+mov al, 0
+; reinicia el contador
+out RCONT, al
 mov al, EOI
 out EOI, al
 iret
@@ -29,8 +49,15 @@ mov al, IMRMASK
 out IMR, al
 mov al, TIMERID
 out TIMER, al
-mov al, 0
-out RCONT, al
 mov al, 1
 out RCOMP, al
+mov al, 0
+out RCONT, al
+mov bx, offset minutos1
+mov al, offset finReloj - offset minutos1
 sti
+
+count: jmp count
+
+fin: int 0
+end
