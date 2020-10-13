78718357e51df63bcf62eeaa492ddb6f478a8eb0
priscilafaliani <priscilafaliani@gmail.com>
2020-10-13 02:08:39 -0300

    La razón de mi insomnio funciona
    Falta pulir detalles pero logré lo que quería. ¿Son las 4 am? sí. Me voy a dormir feliz.

--- Practica 2/Ejercicio15.asm
+++ Practica 2/Ejercicio15.asm
@@ -1,7 +1,7 @@
 ; constants
 EOI EQU 20h
 IMR EQU 21h
-IMRMASK EQU 11111100b
+IMRMASK EQU 11111110b
 F10 EQU 24h
 F10ID EQU 24
 TIMER EQU 25h
@@ -11,16 +11,23 @@ RCOMP EQU 11h
 
 ; variables
 org 1000h
-cantN db 0
+cantN dw 0
 num db 0
 
+; int vector set up
+org 96
+dw F10SR
+
+org 100
+dw TIMERSR
 
 ; int subroutines
 org 3000h
 
 ; da inicio al timer
 F10SR: in al, IMR
-xor al, 11111100b
+xor al, 00000011b
+out IMR, al
 ; incia contador del timer
 mov al, 0
 out RCONT, al
@@ -30,34 +37,54 @@ out EOI, al
 iret
 
 ; hace la cuenta regresiva
-TIMERSR: cmp cantN, 0
-jz stopCounting
-cmp [bx], 30h
-jnz print
-
-; imprime el número
-print: push bx
-mov bx, offset num
+TIMERSR: push bx
+mov bx, dx
+; si llego al final del numero
+cmp bx, 32
+pop bx
+jz finCuentaAtras
+push bx
+; si tengo que hacer 0 - 1
+; simula el proceso de pedir al companero (o a los companeros)
+repetir: cmp byte ptr [bx], 30h
+jnz restaSimple
+mov byte ptr [bx], 39h
+dec bx
+; es necesario pedir a otro?
+jmp repetir
+restaSimple: dec byte ptr [bx]
+mov bx, dx
+; el primer digito es cero? entonces lo quito
+cmp byte ptr [bx], 30h
+pop bx
+jnz finCrearNumero
+inc dx
+dec cantN
+finCrearNumero: push bx
+mov bx, dx
 mov al, cantN
 int 7
 pop bx
 ; end of interruption
-stopCounting: mov al, EOI
+finCuentaAtras: mov al, 0
+out RCONT, al
+mov al, EOI
 out EOI, al
 iret
 
 ; lee numeros en la direccion enviada por bx
-; retorna en bx la dirección de fin del arreglo
+; retorna en bx la direccion de fin del arreglo
 ; deja de leer cuando recibe un "enter" (13)
 LEER: push ax
 cmp al, 0
 jz stop
-repetir: int 6
+repetirLeer: int 6
 cmp byte ptr [bx], 13
 jz stop
 inc bx
-jnz repetir
-stop: ret
+jnz repetirLeer
+stop: pop ax
+ret
 
 
 ; PIC set up
@@ -75,18 +102,20 @@ out TIMER, AL
 ; comp
 mov al, 1
 out RCOMP, al
-; lee el número
+; lee el numero
 mov bx, offset num
 call LEER
-; calcula la cantidad de caracteres
+; guarda la direccion de comienzo
+mov dx, offset num
+; guarda la cantidad de numeros
 mov cantN, bx
-sub cantN, offset num
-; queda apuntando al último carácter
+sub cantN, dx
+inc cantN ; para que cuando imprima de el salto de linea
+; queda apuntando al ultimo caracter
 dec bx
 sti
-
-; espera que se inicie la cuenta atrás con f10
+; espera que se inicie la cuenta atras con f10
 lazo: jmp lazo
 
-int 0
-end
+HLT
+END
